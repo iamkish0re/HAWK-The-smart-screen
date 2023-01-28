7305dadcc8156f0b5aac57da6aadcc5923048257
iamkish0re <kishoresreedhar1999@gmail.com>
2023-01-28 12:45:11 +0530

    Bugfix - Browse picture

--- p.py
+++ p.py
@@ -1,8 +1,8 @@
 #HAWK - MINI PROJECT MODEL 
 
 from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
-from imutils.video import VideoStream
+# from imutils.video import FileVideoStream
+# from imutils.video import VideoStream
 from imutils import face_utils
 import numpy as np
 import argparse
